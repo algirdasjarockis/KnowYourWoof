07e3c7d5dab68991d027ad11fe3142a1b9f9a442
Algirdas Jarockis <algirdas.jarockis@gmail.com>
2024-04-11 23:59:07 +0300

    added request and image caching

--- app/src/main/java/com/ajrock/knowyourwoof/App.kt
+++ app/src/main/java/com/ajrock/knowyourwoof/App.kt
@@ -1,6 +1,12 @@
 package com.ajrock.knowyourwoof
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
+import coil.request.CachePolicy
+import coil.util.DebugLogger
 import com.ajrock.knowyourwoof.data.IScoresRepository
 import com.ajrock.knowyourwoof.data.OfflineScoresRepository
 import com.ajrock.knowyourwoof.data.ScoreDatabase
@@ -9,7 +15,9 @@ import com.ajrock.knowyourwoof.data.repository.DogRepository
 import com.ajrock.knowyourwoof.data.repository.IDogRepository
 import com.ajrock.knowyourwoof.viewmodel.QuizViewModel
 import com.ajrock.knowyourwoof.viewmodel.StatsViewModel
+import okhttp3.Cache
 import okhttp3.OkHttpClient
+import org.koin.android.ext.koin.androidApplication
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -17,10 +25,10 @@ import org.koin.core.context.startKoin
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
 import java.util.concurrent.TimeUnit
 
-
-class App : Application() {
+class App: Application(), ImageLoaderFactory {
     override fun onCreate() {
         super.onCreate()
 
@@ -29,6 +37,26 @@ class App : Application() {
             androidContext(this@App)
             modules(appModule)
         }
+    }
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader(this).newBuilder()
+            .memoryCachePolicy(CachePolicy.ENABLED)
+            .memoryCache {
+                MemoryCache.Builder(this)
+                    .maxSizePercent(0.15)
+                    .strongReferencesEnabled(true)
+                    .build()
+            }
+            .diskCachePolicy(CachePolicy.ENABLED)
+            .diskCache {
+                DiskCache.Builder()
+                    .maxSizePercent(0.05)
+                    .directory(cacheDir)
+                    .build()
+            }
+            .logger(DebugLogger())
+            .build()
     }
 }
 
@@ -40,6 +68,12 @@ internal val appModule = module {
     single<IDogCeoApiService> {
         val client = OkHttpClient.Builder()
             .connectTimeout(8, TimeUnit.SECONDS)
+            .cache(
+                Cache(
+                    directory = File(androidApplication().cacheDir, "http_cache"),
+                    maxSize = 20L * 1024L * 1024L
+                )
+            )
             .build()
 
         val retrofit = Retrofit.Builder()
